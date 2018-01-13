501a13dd091c0ddf01fd33ad66f8754ec0ca0448
Guillermo Olivos <gcolivos@gmail.com>
2018-01-13 14:50:55 -0600

    deleted jq scripts, still working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,36 +14,36 @@ $routeProvider
       templateUrl: '/public/views/register.html',
       controller: "registerController as rc"
     })
-    .when('/teachers', {
-      templateUrl: '/public/views/teacher.html',
-      controller: "teacherController as tc"
-    })
-    .when('/teacherSpins',{
-      templateUrl: '/public/views/teacherSpin.html',
-      controller: "teacherController as tc"
-    })
-    .when('/admins', {
-      templateUrl: '/public/views/admin.html',
-      controller: "adminController as ac"
-    })
-    .when('/adminSpins',{
-      templateUrl: '/public/views/adminSpin.html',
-      controller: "adminController as ac"
-    })
-    .when('/permissions',{
-      templateUrl: 'public/views/permissions.html',
-      controller:'adminController as ac'
-    })
+    // .when('/teachers', {
+    //   templateUrl: '/public/views/teacher.html',
+    //   controller: "teacherController as tc"
+    // })
+    // .when('/teacherSpins',{
+    //   templateUrl: '/public/views/teacherSpin.html',
+    //   controller: "teacherController as tc"
+    // })
+    // .when('/admins', {
+    //   templateUrl: '/public/views/admin.html',
+    //   controller: "adminController as ac"
+    // })
+    // .when('/adminSpins',{
+    //   templateUrl: '/public/views/adminSpin.html',
+    //   controller: "adminController as ac"
+    // })
+    // .when('/permissions',{
+    //   templateUrl: 'public/views/permissions.html',
+    //   controller:'adminController as ac'
+    // })
     .when('/redirect', {
       templateUrl: 'public/views/redirect.html'
     })
     .when('/students', {
       templateUrl: '/public/views/student.html',
       controller: "studentController as sc"
-    })
-    .when('/upload', {
-      templateUrl: '/public/views/upload.html',
-      controller: 'uploadController as uc'
+    // })
+    // .when('/upload', {
+    //   templateUrl: '/public/views/upload.html',
+    //   controller: 'uploadController as uc'
     })
     .otherwise({
       redirectTo: '/login'
