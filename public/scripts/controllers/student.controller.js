d4910d502ea2b9aa2b84f236d71560a2a5b7e251
Guillermo Olivos <gcolivos@gmail.com>
2018-01-13 13:52:03 -0600

    Removed slips call from login page, still working

--- public/scripts/controllers/student.controller.js
+++ public/scripts/controllers/student.controller.js
@@ -1,130 +1,118 @@
-googleAuthApp.controller('studentController', ['$http','$scope','$timeout', 'AuthFactory','$location',function($http, $scope, $timeout, AuthFactory, $location){
-console.log('this is the studentController');
+googleAuthApp.controller('studentController', ['$http', '$scope', '$timeout', 'AuthFactory', '$location', function ($http, $scope, $timeout, AuthFactory, $location) {
+  console.log('this is the studentController');
   var vm = this;
   var authFactory = AuthFactory;
 
   authFactory.isLoggedIn()
-  .then(function (response) {
-    if (response.data.status) {
-      vm.displayLogout = true;
-      authFactory.setLoggedIn(true);
-      console.log(response.data);
-      vm.username = response.data.name;
-      vm.email = response.data.email;
-      vm.id = response.data.id;
-      vm.image = response.data.image;
+    .then(function (response) {
+      if (response.data.status) {
+        vm.displayLogout = true;
+        authFactory.setLoggedIn(true);
+        console.log(response.data);
+        vm.username = response.data.name;
+        vm.email = response.data.email;
+        vm.id = response.data.id;
+        vm.image = response.data.image;
 
-      if(response.data.admin !== true && response.data.teacher !== true){
         $location.path('/students');
+
+
+      } else { // is not logged in on server
+        vm.displayLogout = false;
+        authFactory.setLoggedIn(false);
       }
-      else if(response.data.admin && response.data.teacher){
-        $location.path('/admins');
-      }
-      else if(response.data.admin){
-        $location.path('/admins');
-      }
+    },
 
-      console.log('authfact vm.email',vm.email);
-      vm.getStudentSlips();
+    function () {
+      vm.message.text = 'Unable to properly authenticate user';
+      vm.message.type = 'error';
+    });
 
-
-      // vm.homeroom = response.data.homeroom_id;
-    } else { // is not logged in on server
-      vm.displayLogout = false;
-      authFactory.setLoggedIn(false);
-    }
-  },
-
-  function () {
-    vm.message.text = 'Unable to properly authenticate user';
-    vm.message.type = 'error';
-  });
-
-//start get student slips
-  vm.getStudentSlips = function() {
+  //start get student slips
+  vm.getStudentSlips = function () {
     console.log('getstudentslips', vm.email);
     $http({
       method: 'GET',
 
-      url:'/private/student/slips/' + vm.email
+      url: '/private/student/slips/' + vm.email
 
-    }).then(function(response){
-      console.log('in vmgetstudentslips',response);
+    }).then(function (response) {
+      console.log('in vmgetstudentslips', response);
       vm.studentSlips = response.data.length;
       // console.log(response.data);
     });
   };//end of getStudentList
 
- // vm.getStudentSlips();
+  // vm.getStudentSlips();
 
 
 
   //start post student slips
-    vm.postStudentSlip = function() {
-      // vm.modalValue = true;
+  vm.postStudentSlip = function () {
+    // vm.modalValue = true;
 
-      $http({
-        url: '/private/student/check/' + vm.slipKey,
-        method: 'GET'
-      }).then(function success(response){
-        console.log('This is the response',response.data);
-        if(response.data.isNotUsed === true){
-          var objectToSend = {
-            slip_number: vm.slipKey,
-            s: vm.s,
-            o: vm.o,
-            a: vm.a,
-            r: vm.r,
-            student_id: vm.id,
-            date_entered: new Date()
-          };
-           console.log('this is the object I am sending:', objectToSend);
+    $http({
+      url: '/private/student/check/' + vm.slipKey,
+      method: 'GET'
+    }).then(function success(response) {
+      console.log('This is the response', response.data);
+      if (response.data.isNotUsed === true) {
+        var objectToSend = {
+          slip_number: vm.slipKey,
+          s: vm.s,
+          o: vm.o,
+          a: vm.a,
+          r: vm.r,
+          student_id: vm.id,
+          date_entered: new Date()
+        };
+        console.log('this is the object I am sending:', objectToSend);
 
-          $http({
-            method: 'POST',
-            url:'/private/student',
-            data: objectToSend
-          }).then(function success(response){
-            swal({
-              title: "Nice job!",
-              text: "You have successfully entered a SOAR slip! Keep up the good work!",
-              imageUrl: "https://s-media-cache-ak0.pinimg.com/736x/25/b9/24/25b924f1d18fea2c7dfcb26a9905c1e8--funny-emoji-emoji-png.jpg"
-            });
-            vm.getStudentSlips();
-            console.log(response);
-            vm.slipKey = '';
-            vm.s = false;
-            vm.o = false;
-            vm.a = false;
-            vm.r = false;
-
-          }, function failure(response){
-            console.log(response);
-          });
-        }
-        else{
-          //Make this into an alert or a message
-          console.log('Try again! Slip was already used!');
+        $http({
+          method: 'POST',
+          url: '/private/student',
+          data: objectToSend
+        }).then(function success(response) {
           swal({
-            title: "Oops..",
-            text: "Looks like this slip number has already been used!",
-            imageUrl: "http://pix.iemoji.com/images/emoji/apple/ios-9/256/crying-face.png"
+            title: "Nice job!",
+            text: "You have successfully entered a SOAR slip! Keep up the good work!",
+            imageUrl: "https://s-media-cache-ak0.pinimg.com/736x/25/b9/24/25b924f1d18fea2c7dfcb26a9905c1e8--funny-emoji-emoji-png.jpg"
           });
-          // vm.modalValue = false;
+          vm.getStudentSlips();
+          console.log(response);
+          vm.slipKey = '';
+          vm.s = false;
+          vm.o = false;
+          vm.a = false;
+          vm.r = false;
 
-        }
-      }, function failure(response){
+        }, function failure(response) {
+          console.log(response);
+        });
+      }
+      else {
+        //Make this into an alert or a message
+        console.log('Try again! Slip was already used!');
         swal({
           title: "Oops..",
-          text: "Looks like this slip number does not exist!",
+          text: "Looks like this slip number has already been used!",
           imageUrl: "http://pix.iemoji.com/images/emoji/apple/ios-9/256/crying-face.png"
         });
         // vm.modalValue = false;
 
-        console.log(response);
+      }
+    }, function failure(response) {
+      swal({
+        title: "Oops..",
+        text: "Looks like this slip number does not exist!",
+        imageUrl: "http://pix.iemoji.com/images/emoji/apple/ios-9/256/crying-face.png"
       });
-        vm.getStudentSlips();
+      // vm.modalValue = false;
 
-    };//end of postStudentSlips
+      console.log(response);
+    });
+    vm.getStudentSlips();
+
+  };//end of postStudentSlips
 
 }]);
